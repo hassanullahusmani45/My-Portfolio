d1ce0156a33d9d3c2d066b34a30f342406b5832d
Hassan ullah Usmani <hassanullahusmani45@gmail.com>
2025-07-06 22:51:42 +0430

    update the githup link

--- src/components/links/BtnLinks.tsx
+++ src/components/links/BtnLinks.tsx
@@ -5,7 +5,7 @@ import MyResume from '../../assets/ministry_logo.png'
 export default function BtnLinks() {
     return (
         <section className="flex items-center justify-center gap-8 mt-20">
-            <a href="#" className="btn-githup">GitHub<FiGithub className="size-5" /></a>
+            <a href="https://github.com/hassanullahusmani45" className="btn-githup">GitHub<FiGithub className="size-5" /></a>
             <a
                 href={MyResume}
                 download="MyResume.png"
