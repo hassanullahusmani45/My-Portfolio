a63da6a6d9425051794ce2c7f9fbded69e20cb08
Hassan ullah Usmani <hassanullahusmani45@gmail.com>
2025-07-15 07:40:39 +0430

    change the resume file and link.

--- src/components/links/BtnLinks.tsx
+++ src/components/links/BtnLinks.tsx
@@ -1,14 +1,14 @@
 import { FiGithub } from "react-icons/fi";
 import { LuFileDown } from "react-icons/lu";
-import MyResume from '../../assets/ministry_logo.png'
+import Hassanullahusmani_Resume from '../../assets/Hassanullahusmani_Resume.pdf'
 
 export default function BtnLinks() {
     return (
         <section className="flex items-center justify-center gap-8 mt-20">
             <a href="https://github.com/hassanullahusmani45" className="btn-githup">GitHub<FiGithub className="size-5" /></a>
             <a
-                href={MyResume}
-                download="MyResume.png"
+                href={Hassanullahusmani_Resume}
+                download="Hassanullahusmani_Resume.pdf"
                 className="btn-resume">
                 Resume<LuFileDown className="size-5" />
             </a>
